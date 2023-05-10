5455ac31130f41b464371efb2a4e34aa524713ae
Dario Baum <dario-cbr@hotmail.de>
2023-05-10 12:01:57 +0200

    Added bash script for doing most parts of the setup automatically

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+conda env create -f supra.yml
+conda activate supra
+
+python_path=$(which python3)
+supra_path=$(pwd)
+
+ln -s $python_path  /usr/bin/supra-python3
+ln -s $supra_path/"supra-conformer.py" ~/bin/supra-conformer
+ln -s $supra_path/"supra-refine.py" ~/bin/supra-refine
+ln -s $supra_path/"supra-doubles.py" ~/bin/supra-doubles
+echo "export PYTHONPATH=$supra_path" >> ~/.bashrc
