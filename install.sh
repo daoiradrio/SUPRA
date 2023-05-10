bf118ac733fe9c98e7752368913ad30582dbb6e5
Dario B <71521252+schroedingercode@users.noreply.github.com>
2023-05-10 12:09:56 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ conda activate supra
 python_path=$(which python3)
 supra_path=$(pwd)
 
-ln -s $python_path  /usr/bin/supra-python3
+sudo ln -s $python_path /usr/bin/supra-python3
 ln -s $supra_path/"supra-conformer.py" ~/bin/supra-conformer
 ln -s $supra_path/"supra-refine.py" ~/bin/supra-refine
 ln -s $supra_path/"supra-doubles.py" ~/bin/supra-doubles
