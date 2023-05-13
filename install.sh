c0afcb34cf0106f29418208f0d89014895d54920
Dario B <71521252+schroedingercode@users.noreply.github.com>
2023-05-13 14:41:44 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-conda env create -f supra.yml
-conda activate supra
-
 supra_path=$(pwd)
 
+conda env create -f supra.yml
 sudo ln -s $HOME/miniforge3/envs/supra/bin/python3 /usr/bin/supra-python3
 ln -s $supra_path/"supra-conformer.py" ~/bin/supra-conformer
 ln -s $supra_path/"supra-refine.py" ~/bin/supra-refine
