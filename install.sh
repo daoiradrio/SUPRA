160c888481ac18916c1b5a2abc28782420a60360
Dario Baum <dario-cbr@hotmail.de>
2023-06-27 14:20:59 +0200

    Adjusted install script

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ supra_path=$(pwd)
 conda env create -f supra.yml
 sudo ln -s $HOME/miniforge3/envs/supra/bin/python3 /usr/bin/supra-python3
 ln -s $supra_path/"supra-conformer.py" ~/bin/supra-conformer
-ln -s $supra_path/"supra-refine.py" ~/bin/supra-refine
 ln -s $supra_path/"supra-doubles.py" ~/bin/supra-doubles
+ln -s $supra_path/"supra-compare.py" ~/bin/supra-compare
+ln -s $supra_path/"supra-refine.py" ~/bin/supra-refine
+print(os.path.dirname(path))
 echo "export PYTHONPATH=$supra_path" >> ~/.bashrc
 #echo "export TURBODIR=$HOME/bin/TURBOMOLE" >> ~/.bashrc
 #echo "source $TURBODIR/Config_turbo_env" >> ~/.bashrc
