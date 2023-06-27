229ea613102cf23dc8215a31fe33e1988febc6a5
Dario Baum <dario-cbr@hotmail.de>
2023-06-27 09:31:21 +0200

    New conda environment file (including rdkit)

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ ln -s $supra_path/"supra-conformer.py" ~/bin/supra-conformer
 ln -s $supra_path/"supra-refine.py" ~/bin/supra-refine
 ln -s $supra_path/"supra-doubles.py" ~/bin/supra-doubles
 echo "export PYTHONPATH=$supra_path" >> ~/.bashrc
-echo "export TURBODIR=$HOME/bin/TURBOMOLE" >> ~/.bashrc
-echo "source $TURBODIR/Config_turbo_env" >> ~/.bashrc
+#echo "export TURBODIR=$HOME/bin/TURBOMOLE" >> ~/.bashrc
+#echo "source $TURBODIR/Config_turbo_env" >> ~/.bashrc
