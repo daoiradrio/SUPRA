1f3af3e64c9ffd1d1393f15166f4d68c404d8f15
Dario Baum <dario-cbr@hotmail.de>
2023-08-01 09:34:36 +0200

    Added tests

--- tests/test_symmetry.py
+++ tests/test_symmetry.py
@@ -1,7 +1,10 @@
 import os
 
-from utils.helper import get_number
+import numpy as np
+
+from utils.helper import get_number, geometric_center
 from utils.symmetry import Symmetry
+from utils.rotationaxis import RotationAxis
 from SUPRAConformer.structure import Structure
 from SUPRAConformer.conformergenerator import ConformerGenerator
 
@@ -51,7 +54,41 @@ def test_find_torsion_group():
 
 
 def test_rot_sym_along_bond():
-    assert True
+    sym = Symmetry()
+    mol1 = Structure(os.path.join(files, "Alanin.xyz"))
+    mol2 = Structure(os.path.join(files, "Benzol.xyz"))
+
+    atoms = ["C2", "H3", "H4", "H5"]
+    from_coords = mol1.coords["C0"]
+    to_coords = mol1.coords["C2"]
+    rotaxis1 = RotationAxis(from_coords, to_coords)
+
+    check1 = sym.rot_sym_along_bond(mol1, rotaxis1, atoms, 3)
+    check2 = sym.rot_sym_along_bond(mol1, rotaxis1, atoms, 2)
+
+    atoms = ["N6", "H8", "H9"]
+    from_coords = mol1.coords["C0"]
+    to_coords = mol1.coords["N6"]
+    rotaxis2 = RotationAxis(from_coords, to_coords)
+
+    check3 = sym.rot_sym_along_bond(mol1, rotaxis2, atoms, 1)
+    check4 = sym.rot_sym_along_bond(mol1, rotaxis2, atoms, 2)
+
+    atoms = list(mol2.coords.keys())
+    from_coords = geometric_center(mol2.coords)
+    vec1 = (from_coords - mol2.coords["C0"])
+    vec1 = vec1 / np.linalg.norm(vec1)
+    vec2 = (from_coords - mol2.coords["C1"])
+    vec2 = vec2 / np.linalg.norm(vec2)
+    norm_vec = np.cross(vec1, vec2)
+    norm_vec = norm_vec / np.linalg.norm(norm_vec)
+    to_coords = from_coords + norm_vec
+    rotaxis3 = RotationAxis(from_coords, to_coords)
+
+    check5 = sym.rot_sym_along_bond(mol2, rotaxis3, atoms, 6)
+    check6 = sym.rot_sym_along_bond(mol2, rotaxis3, atoms, 3)
+
+    assert (check1 and not check2 and check3 and not check4 and check5 and check6)
 
 
 
